16a633214cc6b2ddf209265e99e928f119098a31
Roman Choporov <ChoporovRoman@gmail.com>
2025-02-07 15:20:17 -0800

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import router as contact_router
 
 app = FastAPI()
+origins = ["<http://localhost:3000>"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include the API routes
 app.include_router(contact_router)
+
 
 # Optional: Healthcheck or any other root endpoints
 @app.get("/")
